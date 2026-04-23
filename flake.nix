471a26b739c477addc7c09eaefdd5731299f4f9d
somya <somyasati14@gmail.com>
2026-04-23 11:54:24 +0300

    added flakes

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Nix flake providing devShell and build for Rust project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.cargo
+            pkgs.rust-analyzer
+            pkgs.cargo-watch
+            pkgs.cargo-edit
+          ];
+        };
+      });
+}
